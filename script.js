b115a68c0788b047fa1659c92ceaa149cfe4197e
Thato Lesebo <thatolesebo@gmail.com>
2025-06-14 17:14:51 +0200

    Script.js - Operator button

--- script.js
+++ script.js
@@ -5,6 +5,13 @@ const display = document.getElementById('display');
 
 let justCalculated = false;
 
+function isOperator(char){
+    return ['+','-','*','/'].includes(char);
+}
+function getLastChar() {
+    return display.value.slice(-1);
+}
+
 function appendToDisplay(value) {
     console.log('Button pressed:', value);
     let currentValue = display.value;
@@ -13,20 +20,48 @@ function appendToDisplay(value) {
         justCalculated = false;
         return;
     }
-    // if current display shows zero and user enters a number we wanna replace the zero
 
-    if (currentValue === "0" && !isNaN(value)){
-        display.value = value;
-    //I the current display shows zero and the user enters decimal, keep the zero
-    }else if (currentValue === '0' && value ==='.') {
+    if (justCalculated && isOperator (value)){
         display.value = currentValue + value;
-    }else if (value === '.'){
-        // Get the last number in the display
-        let lastNumber = currentValue.split('/[+\-*/]').pop();
-        //Only add the decimal if the current number doesnt have ont
-        if (!lastNumber.includes('.')) {
+        justCalculated = false;
+        return;
+    }
+    //Handles operators
+
+    if (isOperator(value)){
+        //Dont allow operator as first char, (exception for minus)
+        if (currentValue === '0' && value!== '-'){
+            return; //Do nothing
+        }
+
+        //If the last charactor is already an operator, repla
+        // ce it
+        if (isOperator(getLastChar ())) {
+            display.value = currentValue.slice(0,-1) + value;
+        } else {
+            display.value = currentValue + value;
+        } // if current display shows zero and user enters a number we wanna replace the zero
+    }else if (!isNaN(value)){ 
+        if (currentValue === '0'){
+            display.value = value;
+        } else {
             display.value = currentValue + value;
         }
+
+    //I the current display shows zero and the user enters decimal, keep the zero
+    }else if (value === '.') {
+        if (currentValue === '0'){;
+        display.value = currentValue = value;
+    }else {
+        //Get the last number in the display after the last operator
+        let parts = currentValue.split('/[+\-*/]');
+        let lastNumber = parts[parts.length -1];
+        //Only add decimal if number doesnt already have one
+
+        if (lastNumber.includes('.')){
+            display.value = currentValue + value;
+        }
+    }
     }else {
         display.value = currentValue + value;
     }
